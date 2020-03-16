a09f2e48d809dfd5f993f77d82c68f96c1c5013c
Dillan Teagle <dteagle@cpicardgroup.com>
2020-03-16 08:09:26 -0500

    :memo: adding doctests

--- cookbook/classes/attributes.py
+++ cookbook/classes/attributes.py
@@ -12,4 +12,31 @@ class ShippingContainer:
         ShippingContainer.next_serial += 1
 
 
-# ShippingContainer(33, "something")
+class ShippingContainerTwo:
+    """class attribute"""
+    next_serial = 1337
+
+    def __init__(self, owner_code, contents):
+        """instance attribute"""
+        self.owner_code = owner_code
+        self.contents = contents
+        self.serial = self.next_serial
+        self.serial += 1
+
+
+__test__ = {
+    'ShippingContainer': '''
+        >>> ShippingContainer(33, "something").serial
+        1337
+        >>> ShippingContainer(33, "something").serial
+        1338
+    ''',
+    'ShippingContainerTwo': '''
+        >>> ShippingContainerTwo(34, "something").serial
+        1338
+    '''
+}
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
